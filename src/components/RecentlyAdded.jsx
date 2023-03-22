cc78b35b408ede6f5689edbd46d83b0e35d6c022
unknown <vsagar267@gmai;.com>
2023-03-22 22:09:49 +0530

    recently added page improved

--- src/components/RecentlyAdded.jsx
+++ src/components/RecentlyAdded.jsx
@@ -1,18 +1,21 @@
 import React from "react";
+import Row from "./Row";
+import { request } from "./requests";
+import Footer from "./Footer";
 
 const RecentlyAdded = () => {
   return (
     <>
       <div>
-        <div className="bg-black h-[100vh]">
-          <div >
+        <div className="bg-black h-full">
+          <div className="pb-12">
             <img
               src="https://images.pexels.com/photos/1386506/pexels-photo-1386506.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
               alt="movie img"
               className="object-cover w-full h-[80vh] no-repeat relative"
             />
             <div className="absolute w-full h-[80vh] top-0 bg-gradient-to-b from-[#00000018] to-[#000000d5] "></div>
-            <div className="text-white flex flex-row absolute top-[28vh] ml-[260px]">
+            <div className="text-white flex flex-row absolute top-[28vh] ml-[260px] ">
               <img
                 src="https://images.pexels.com/photos/15094547/pexels-photo-15094547.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                 alt="movie img"
@@ -41,6 +44,13 @@ const RecentlyAdded = () => {
               </div>
             </div>
           </div>
+          <div><Row
+            rowID="2"
+            title="Top Rated Movies"
+            fetchURL={request.requestTopRatedMovies}
+          /></div>
+          <div className="pb-32"><Row rowID = "4" title="Popular Movies" fetchURL={request.requestPopularMovies}/></div>
+          <Footer/>
         </div>
       </div>
     </>
